ab3f7301bfbc25eafb05eb96c18776f5caf2ac8e
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-07-30 22:05:55 +0200

    react

---
+++ src/components/Page2/DropdownItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+
+export default function DropdownItem() {
+    return (
+       <div>
+
+      <button className=''>
+        navigate
+      </button>
+
+       </div> 
+    )
+}
--- src/components/Page2/Navbar.tsx
+++ src/components/Page2/Navbar.tsx
@@ -1,10 +1,12 @@
+import DropdownItem from "./DropdownItem";
 export default function Navbar() {
     return (
         <div>
             <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
                     <p className="flex items-center">
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Full Stack Squad</span>
+                        <span className="self-center text-5xl font-extrabold whitespace-nowrap dark:text-white ">METASIST.</span>
+                        <DropdownItem/>
                     </p>
                     <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
